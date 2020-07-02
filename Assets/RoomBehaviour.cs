5db7a6118d983aad6082cd7fdba536d440c411c7
Atlas <AtlasTheTitan77@gmail.com>
2020-07-02 23:05:13 +0200

    Init

---
+++ Assets/RoomBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehaviour : MonoBehaviour
+{
+    public List<Transform> doors = new List<Transform>();
+    public List<BoxCollider> colliders = new List<BoxCollider>();
+
+    public bool CheckRoom(Vector3 newPositon, LayerMask layer) {
+        foreach(BoxCollider col in colliders) {
+            Vector3 pos = newPositon + col.center;
+            if(Physics.CheckBox(pos, col.size/2, transform.rotation, layer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
